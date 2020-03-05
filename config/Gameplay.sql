bd59b86860005690482d9af549bb9911d6b033a1
lofersfanclub <lindahl.jonatan@gmail.com>
2020-03-05 17:42:21 +0100

    sql

---
+++ config/Gameplay.sql
@@ -0,0 +1,16 @@
+create table Gameplay (
+	id INT,
+	name VARCHAR(50),
+	email VARCHAR(50),
+	score INT
+);
+insert into Gameplay (id, name, email, score) values (1, 'Clareta', 'cassante0@github.io', 17);
+insert into Gameplay (id, name, email, score) values (2, 'Tommi', 'tmortel1@creativecommons.org', 10);
+insert into Gameplay (id, name, email, score) values (3, 'Bobby', 'bmomery2@reddit.com', 6);
+insert into Gameplay (id, name, email, score) values (4, 'Rand', 'rhoofe3@newyorker.com', 10);
+insert into Gameplay (id, name, email, score) values (5, 'Joeann', 'jdupey4@4shared.com', 13);
+insert into Gameplay (id, name, email, score) values (6, 'Lyn', 'laylwin5@51.la', 14);
+insert into Gameplay (id, name, email, score) values (7, 'Montague', 'mhulse6@seattletimes.com', 12);
+insert into Gameplay (id, name, email, score) values (8, 'Miguel', 'mbernardelli7@amazon.co.jp', 13);
+insert into Gameplay (id, name, email, score) values (9, 'Jud', 'jdecourcey8@go.com', 7);
+insert into Gameplay (id, name, email, score) values (10, 'Augusta', 'adabels9@naver.com', 13);
